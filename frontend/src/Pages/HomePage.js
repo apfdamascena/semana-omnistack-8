d11f52299f656991cae4dff0ddaf24e9e2891096
apfdamascena <apfd@cin.ufpe.br>
2020-07-26 18:42:03 -0300

    starting frontend components

---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import tindevLogo from '../Images/logo.svg'
+
+export default function HomePage() {
+    return (
+        <img src = {tindevLogo} alt = "Tindev Logo"/>
+    );
+}
