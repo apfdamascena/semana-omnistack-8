aebc5bf79f362027cf2469abb7d6a62b568ce96b
apfdamascena <apfd@cin.ufpe.br>
2020-07-25 21:00:27 -0300

    databse ok

--- backend/src/controllers/developersController.js
+++ backend/src/controllers/developersController.js
@@ -1,9 +1,23 @@
 const axios = require('axios');
+const developers = require('../models/developers');
 
 module.exports = {
     async store(serverRequest, serverAnswer){
         const {username} = serverRequest.body;
+        const doesTheUserExist = await developers.findOne({user: username});
         const dataUserReference = await axios.get(`https://api.github.com/users/${username}`);
-        return serverAnswer.json(dataUserReference.data);
+
+        if(doesTheUserExist){
+            return serverAnswer.json(doesTheUserExist);
+        }
+
+        const {name, bio: biography, avatar_url: avatar} = dataUserReference.data;
+        const DeveloperInfo = await developers.create({
+            name,
+            user: username,
+            biography,
+            avatar
+        })
+        return serverAnswer.json(DeveloperInfo);
     }
 }
