944f2de8e59748457b4e01f3163b8f4f57666a3e
apfdamascena <apfd@cin.ufpe.br>
2020-07-28 04:51:08 -0300

    connecting almost done

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MainPage.css';
 
-export default function MainPage({match}){
-    return(
-        <h2>{match.params.id}</h2>
+import API from '../../Services/api';
+
+import logo from '../../Images/logo.svg';
+import like from '../../Images/like.svg';
+import dislike from '../../Images/dislike.svg';
+
+
+export default function MainPage({ match }) {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await API.get('/dev', {
+                headers: {
+                    user: match.params.id,
+                }
+            })
+            setUsers(response.data);
+        }
+        loadUsers();
+    }, [match.params.id])
+
+    return (
+        <div className="mainContainer">
+            <img src={logo} alt="Tindev Logo" />
+            <ul>
+                {users.map(user => (
+                    <li>
+                        <img src= {user.avatar} alt = {user.name}/>
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
+                        <div className="likeOrDislikeButtons">
+                            <button type="button">
+                                <img src={like} alt="like" />
+                            </button>
+                            <button type="button">
+                                <img src={dislike} alt="dislike" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
